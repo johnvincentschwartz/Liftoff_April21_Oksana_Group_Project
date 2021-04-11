a0ecce627726fb22d0f96ffbfa500d35773a8c01
Jermell <afootball.life26@gmail.com>
2021-04-11 18:31:05 -0500

    Hello World Update

---
+++ src/test/java/launchcode/liftoff_project/LiftoffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package launchcode.liftoff_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
