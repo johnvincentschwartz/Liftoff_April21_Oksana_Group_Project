da8f73acadbe8782d35272f8cada9d6ba0139180
John Schwartz <johnvincentschwartz@gmail.com>
2021-04-23 18:01:40 -0500

    filter by min and max distance functional

--- src/main/java/launchcode/liftoff_project/Controllers/TrailController.java
+++ src/main/java/launchcode/liftoff_project/Controllers/TrailController.java
@@ -1,12 +1,18 @@
 package launchcode.liftoff_project.Controllers;
 
+import launchcode.liftoff_project.Model.Trail;
 import launchcode.liftoff_project.Model.data.TrailRepository;
 import org.hibernate.annotations.SQLInsert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("alltrails")
@@ -18,8 +24,36 @@ public class TrailController {
 
     @GetMapping
     public String index(Model model){
-        model.addAttribute("allTrails", trailRepository.findAll());
+
+        model.addAttribute("trails", trailRepository.findAll());
+
         return "alltrails";
+    }
+
+    @PostMapping("results")
+    public String displayFilterResults(Model model, @RequestParam Double minLength, @RequestParam Double maxLength){
+        Iterable<Trail> allTrails = trailRepository.findAll();
+        Iterable<Trail> results = filterTrailsLength(minLength, maxLength, allTrails);
+
+        model.addAttribute("trails", results);
+
+        return "alltrails";
+
+    }
+
+    public static ArrayList<Trail> filterTrailsLength(Double minLength, Double maxLength, Iterable<Trail> allTrails){
+        ArrayList<Trail> results = new ArrayList<>();
+
+        if (minLength == null){ minLength = 0.0; }
+        if (maxLength == null){ maxLength = 1000.0;}
+
+        for (Trail trail : allTrails){
+            if (trail.getLength() > minLength && trail.getLength() < maxLength) {
+                results.add(trail);
+            }
+        }
+
+        return results;
     }
 
 }
