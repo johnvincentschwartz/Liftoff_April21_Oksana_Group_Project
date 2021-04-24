4c6ed74d7225e31b9e1687ff0a6b1cfbffe8aa2b
John Schwartz <johnvincentschwartz@gmail.com>
2021-04-24 08:08:58 -0500

    filter by difficulty enabled, cleaned up alltrails template

--- src/main/java/launchcode/liftoff_project/Controllers/TrailController.java
+++ src/main/java/launchcode/liftoff_project/Controllers/TrailController.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.SQLInsert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,9 +32,11 @@ public class TrailController {
     }
 
     @PostMapping("results")
-    public String displayFilterResults(Model model, @RequestParam Double minLength, @RequestParam Double maxLength){
+    public String displayFilterResults(Model model, @RequestParam Double minLength, @RequestParam Double maxLength,
+                                       @RequestParam List<Integer> difficulty, @RequestParam String sort){
         Iterable<Trail> allTrails = trailRepository.findAll();
-        Iterable<Trail> results = filterTrailsLength(minLength, maxLength, allTrails);
+        Iterable<Trail> results = filterTrails(minLength, maxLength, difficulty, sort, allTrails);
+        System.out.println(difficulty);
 
         model.addAttribute("trails", results);
 
@@ -41,17 +44,45 @@ public class TrailController {
 
     }
 
-    public static ArrayList<Trail> filterTrailsLength(Double minLength, Double maxLength, Iterable<Trail> allTrails){
+    public static ArrayList<Trail> filterTrails(Double minLength, Double maxLength, List<Integer> difficulty, String sort, Iterable<Trail> allTrails){
         ArrayList<Trail> results = new ArrayList<>();
 
         if (minLength == null){ minLength = 0.0; }
         if (maxLength == null){ maxLength = 1000.0;}
+        if (difficulty == null){
+            difficulty = new ArrayList<>();
+            difficulty.add(1);
+            difficulty.add(2);
+            difficulty.add(3);
+            difficulty.add(4);
+            difficulty.add(5);
+        }
+
+
+        System.out.println(difficulty);
 
         for (Trail trail : allTrails){
-            if (trail.getLength() > minLength && trail.getLength() < maxLength) {
+            if (
+                trail.getLength() > minLength && trail.getLength() < maxLength
+                && difficulty.contains(trail.getDifficulty())) {
                 results.add(trail);
             }
         }
+
+        if (sort.equals("length-asc")){
+
+        } else if (sort.equals("length-desc")){
+
+        } else if (sort.equals("difficulty-asc")) {
+
+        } else if (sort.equals("difficulty-desc")){
+
+        }
+
+
+
+
+
 
         return results;
     }
