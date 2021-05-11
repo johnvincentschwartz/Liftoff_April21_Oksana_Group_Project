238416e71360a6f1b8c23fec1f54072151b989c0
John Schwartz <johnvincentschwartz@gmail.com>
2021-05-11 12:40:40 -0500

    filter by dog- family- and bike-friendly

--- src/main/java/launchcode/liftoff_project/Controllers/TrailController.java
+++ src/main/java/launchcode/liftoff_project/Controllers/TrailController.java
@@ -1,5 +1,6 @@
 package launchcode.liftoff_project.Controllers;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import launchcode.liftoff_project.Model.Trail;
 import launchcode.liftoff_project.Model.data.TrailRepository;
 import org.hibernate.annotations.SQLInsert;
@@ -17,7 +18,6 @@ import java.util.*;
 
 @Controller
 @RequestMapping("alltrails")
-
 public class TrailController {
 
     @Autowired
@@ -33,8 +33,12 @@ public class TrailController {
         difficulty.add(4);
         difficulty.add(5);
 
+
         model.addAttribute("trails", trailRepository.findAll());
         model.addAttribute("difficulty", difficulty);
+        model.addAttribute("requireDogs", false);
+        model.addAttribute("requireKids", false);
+        model.addAttribute("requireBikes", false);
 
         return "alltrails";
     }
@@ -42,15 +46,25 @@ public class TrailController {
     @PostMapping()
     public String displayFilterResults(Model model, @RequestParam Double minLength, @RequestParam Double maxLength,
                                        @RequestParam List<Integer> difficulty, @RequestParam String searchLocation,
-                                       @RequestParam String sort){
+                                       @RequestParam(name="requireDogs", required=false) Boolean requireDogs,
+                                       @RequestParam(name="requireKids", required=false) Boolean requireKids,
+                                       @RequestParam(name="requireBikes", required=false) Boolean requireBikes, @RequestParam String sort){
 
         Iterable<Trail> allTrailsSorted = trailRepository.findAll(Sort.by(Sort.Direction.ASC, sort));
-        Collection<Trail> results = filterTrails(minLength, maxLength, difficulty, allTrailsSorted);
+
+        if (requireDogs == null){requireDogs = false;}
+        if (requireKids == null){requireKids = false;}
+        if (requireBikes == null){requireBikes = false;}
+
+        Collection<Trail> results = filterTrails(minLength, maxLength, difficulty, allTrailsSorted, requireDogs, requireKids, requireBikes);
 
         model.addAttribute("minLength", minLength);
         model.addAttribute("maxLength", maxLength);
         model.addAttribute("difficulty", difficulty);
         model.addAttribute("searchLocation", searchLocation);
+        model.addAttribute("requireDogs", requireDogs);
+        model.addAttribute("requireKids", requireKids);
+        model.addAttribute("requireBikes", requireBikes);
         model.addAttribute("sort", sort);
         model.addAttribute("trails", results);
 
@@ -58,7 +72,9 @@ public class TrailController {
 
     }
 
-    public static ArrayList<Trail> filterTrails(Double minLength, Double maxLength, List<Integer> difficulty, Iterable<Trail> allTrails){
+    public static ArrayList<Trail> filterTrails(Double minLength, Double maxLength, List<Integer> difficulty, Iterable<Trail> allTrails,
+                                                Boolean requireDogs, Boolean requireKids, Boolean requireBikes){
+
         ArrayList<Trail> results = new ArrayList<>();
 
         if (minLength == null){ minLength = 0.0; }
@@ -73,9 +89,15 @@ public class TrailController {
         }
 
         for (Trail trail : allTrails){
+            if (
+                (requireDogs && !trail.getDogs()) ||
+                (requireKids && !trail.getFamily()) ||
+                (requireBikes && !trail.getBikes())
+            ) {continue;}
 
             if (
-                trail.getLength() > minLength && trail.getLength() < maxLength
+                trail.getLength() > minLength
+                && trail.getLength() < maxLength
                 && difficulty.contains(trail.getDifficulty())) {
                 results.add(trail);
             }
