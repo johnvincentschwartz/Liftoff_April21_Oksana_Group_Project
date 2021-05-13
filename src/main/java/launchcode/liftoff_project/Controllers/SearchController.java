bcecdc393f2da275aa0c23c74235b3bac46d8bf6
Jermell <afootball.life26@gmail.com>
2021-05-13 12:49:14 -0500

    Search Function

---
+++ src/main/java/launchcode/liftoff_project/Controllers/SearchController.java
@@ -0,0 +1,34 @@
+package launchcode.liftoff_project.Controllers;
+
+import launchcode.liftoff_project.Model.Trail;
+import launchcode.liftoff_project.Model.data.TrailRepository;
+import launchcode.liftoff_project.Model.TrailData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import launchcode.liftoff_project.Controllers.TrailController;
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @Autowired
+    private TrailRepository trailRepository;
+
+    @RequestMapping("")
+    public String search(Model model){
+        model.addAttribute("trails", trailRepository.findAll());
+        return "search";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+        Iterable<Trail> trails;
+        trails = TrailData.searchValue(searchTerm, trailRepository.findAll());
+        model.addAttribute("trails", trails );
+        return "search";
+    }
+}
