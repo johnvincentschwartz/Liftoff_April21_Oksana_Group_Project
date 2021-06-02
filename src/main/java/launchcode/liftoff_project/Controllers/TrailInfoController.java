58bfa2beb8ed01db0cbb29a13e8f91191e4bd960
John Schwartz <johnvincentschwartz@gmail.com>
2021-06-02 15:40:14 -0500

    individual trail page routing

--- src/main/java/launchcode/liftoff_project/Controllers/TrailInfoController.java
+++ src/main/java/launchcode/liftoff_project/Controllers/TrailInfoController.java
@@ -1,11 +1,15 @@
 package launchcode.liftoff_project.Controllers;
 
+import launchcode.liftoff_project.Model.Trail;
 import launchcode.liftoff_project.Model.data.TrailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class TrailInfoController {
@@ -13,32 +17,18 @@ public class TrailInfoController {
     @Autowired
     private TrailRepository trailRepository;
 
-    @GetMapping("trailOne")
-    public String displayTrailInfoPage(Model model){
+    @GetMapping("trail/{id}")
+    public String displayTrailInfoPage(Model model, @PathVariable int id){
 
-        model.addAttribute("trailOne", "trailOne");
-        model.addAttribute("trails", trailRepository.findAll());
-        return "trailOne";
+        Optional<Trail> trailOpt = trailRepository.findById(id);
+        Trail trail = null;
+
+        if (trailOpt.isPresent()){
+            trail = trailOpt.get();
+        }
+
+        model.addAttribute("trail", trail);
+        return "trail";
     }
 
-    @GetMapping("trailTwo")
-    public String trailInfoTwo(Model model){
-
-        model.addAttribute("trailTwo", "trailTwo");
-        return "trailTwo";
-    }
-
-    @GetMapping("trailThree")
-    public String trailInfoThree(Model model){
-
-        model.addAttribute("trailThree", "trailThree");
-        return "trailThree";
-    }
-
-    @GetMapping("trailFour")
-    public String trailInfoFour(Model model){
-
-        model.addAttribute("trailFour", "trailFour");
-        return "trailFour";
-    }
 }
